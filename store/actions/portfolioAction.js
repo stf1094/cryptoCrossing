2e3042940c72d2fab9bfd8312d88679e330dbe09
SF <stf1094@gmail.com>
2023-11-01 20:17:55 -0400

    change market to top 200 coins

--- store/actions/portfolioAction.js
+++ store/actions/portfolioAction.js
@@ -153,7 +153,7 @@ export const getMarket = () => async dispatch => {
         }
       }
       try {
-        const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d&locale=en&precision=2')
+        const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d&locale=en&precision=2')
         .then((res) => res.json())
         .then((data) => {
             data.forEach((item) => {
